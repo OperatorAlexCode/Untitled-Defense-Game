f259a0018b5cf80cb5376c85a86368c60affa864
julian-mau <89749274+julian-mau@users.noreply.github.com>
2023-04-10 20:25:46 +0200

    Miner (Resource nodes) and UI
    Added:
    - Mining scene.
    - Controllable player (miner).
    - Interactable resource nodes.
    - Gold and gunpowder UI.

--- Assets/Scripts/ResourceNode.cs
+++ Assets/Scripts/ResourceNode.cs
@@ -10,7 +10,6 @@ public class ResourceNode : MonoBehaviour
     int MinerLevel;
     ResourceType Type;
     bool IsMined;
-
     
     // Start is called before the first frame update
     void Start()
