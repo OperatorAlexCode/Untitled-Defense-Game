7ccbe5da64606a7283592ab21060ce3c2e551ef8
OperatorAlexCode <alexander.jr.karlsson@gmail.com>
2023-04-14 10:51:18 +0200

    Enemies now have new models. Work has begun to allow cannon to be upgraded

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -28,6 +28,11 @@ public class CannonController : MonoBehaviour
     float CurrentAngle;
     float CurrentRotation;
 
+    // Int
+    int[] DamageUpgradeLevel;
+    int[] KnockBackUpgradeLevel;
+    public int[] ReserveAmmo;
+
     // GameObject
     public GameObject Turret;
     public GameObject Barrel;
@@ -45,7 +50,6 @@ public class CannonController : MonoBehaviour
     Vector3 TurretStartRotation;
 
     // Other
-    public int[] ReserveAmmo;
     public Material CannonBallMat;
     ShotType CurrentShot;
 
@@ -63,6 +67,8 @@ public class CannonController : MonoBehaviour
         TurretStartRotation = Turret.transform.eulerAngles;
 
         Cooldowns = new float[MaxCooldowns.Length];
+        DamageUpgradeLevel = new int[] { 1, 1 };
+        KnockBackUpgradeLevel = new int[] { 1, 1 };
     }
 
     // Update is called once per frame
@@ -278,10 +284,11 @@ public class CannonController : MonoBehaviour
         rigidBody.drag = ProjectileDrag[shotType];
         rigidBody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
 
+        // Adds damage and knockback to the projectile
         cannonBall.AddComponent<ProjectileController>();
         cannonBall.GetComponent<ProjectileController>().LifeTime = ProjectileLifetime[shotType];
-        cannonBall.GetComponent<ProjectileController>().Damage = Damage[shotType];
-        cannonBall.GetComponent<ProjectileController>().knockBack = KnockBack[shotType];
+        cannonBall.GetComponent<ProjectileController>().Damage = Damage[shotType] * (1 + (DamageUpgradeLevel[shotType] - 1 / 10));
+        cannonBall.GetComponent<ProjectileController>().knockBack = KnockBack[shotType] * (1 + (KnockBackUpgradeLevel[shotType] - 1 / 10));
 
         return cannonBall;
     }
@@ -289,6 +296,16 @@ public class CannonController : MonoBehaviour
     float GetRandomNumber(float range)
     {
         return Random.Range(-range, range);
+    }
+
+    public void UpgradeDamage(int shotToUpgrade)
+    {
+        DamageUpgradeLevel[shotToUpgrade] += 1;
+    }
+
+    public void UpgradeknockBack(int shotToUpgrade)
+    {
+        KnockBackUpgradeLevel[shotToUpgrade] += 1;
     }
 }
 
