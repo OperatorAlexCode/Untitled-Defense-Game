7d9bdb9593dedd6e2f514816f968a8c1ce7ea9a0
julian-mau <89749274+julian-mau@users.noreply.github.com>
2023-04-18 10:04:42 +0200

    Day/Night cycle and mine damage

--- Assets/Mine.cs
+++ Assets/Mine.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mine : MonoBehaviour
 {
+    public float health;
+    public Slider slider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class Mine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        slider.value = health;
+
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.gameObject.tag == "Enemy")
+            health = health - 100f;
     }
 }
---
+++ Assets/Scripts/WaveCycle.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveCycle : MonoBehaviour
+{
+    public Light sun;
+    public GameObject Camera1;
+    public GameObject Camera2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraOne();
+        sun.GetComponent<Light>().enabled = true;
+        Invoke("TurnSunOff", 3);
+        Invoke("TurnSunOn", 6);
+    }
+
+    public void TurnSunOn()
+    {
+        sun.GetComponent<Light>().enabled = true;
+        CameraOne();
+    }
+
+    public void TurnSunOff()
+    {
+        sun.GetComponent<Light>().enabled = false;
+        CameraTwo();
+    }
+
+    public void CameraOne()
+    {
+        Camera1.SetActive(true);
+        Camera2.SetActive(false);
+    }
+
+    public void CameraTwo()
+    {
+        Camera1.SetActive(false);
+        Camera2.SetActive(true);
+    }
+}
