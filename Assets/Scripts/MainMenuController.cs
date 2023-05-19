fdd90f3c2afe4d41599ffe862a39b0b3edae7f38
julian-mau <89749274+julian-mau@users.noreply.github.com>
2023-05-19 13:47:31 +0200

    Added settings menu, player can now go to settings menu from the main menu
    Co-Authored-By: OperatorAlexCode <96977755+OperatorAlexCode@users.noreply.github.com>

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -8,6 +8,7 @@ public class MainMenuController : MonoBehaviour
     void Awake()
     {
         GameObject.Find("Start").gameObject.GetComponent<Button>().onClick.AddListener(SceneController.LoadGameScene);
+        GameObject.Find("Settings").gameObject.GetComponent<Button>().onClick.AddListener(SceneController.LoadSettingsMenuScene);
         GameObject.Find("Exit").gameObject.GetComponent<Button>().onClick.AddListener(SceneController.ExitApplication);   
     }
 }
